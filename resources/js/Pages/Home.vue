1fd277ddaee21016d6e3059e3f526f999a93f1e3
kyawzayarlynn <zipzep327@gmail.com>
2024-04-27 18:13:01 +0630

    setup laravel,vue and inertia

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Hello World {{ name }}</h1>
+  <p>In this series we will use following frameworks</p>
+  <ul>
+    <li v-for="framework in frameworks" v-text="framework" :key="framework"></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    frameworks: Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
