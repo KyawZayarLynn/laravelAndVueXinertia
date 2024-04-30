02b5e8e12568390db2a7a3c67d3744aa4c1c6f51
kyawzayarlynn <zipzep327@gmail.com>
2024-04-30 12:53:18 +0630

    persistent layout

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,21 +1,21 @@
 <template>
-  <Layout>
-    <h1 class="text-3xl">Users</h1>
-    <div style="margin-top: 600px;">
-      <p>The current time is {{ time }}</p>
-      <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
-    </div>
-  </Layout>
+  <h1 class="text-3xl">Users</h1>
+  <div style="margin-top: 600px;">
+    <p>The current time is {{ time }}</p>
+    <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
+  </div>
 </template>
 
-<script setup>
+<script>
 
 import Layout from '../Shared/Layout'
-import { Link } from '@inertiajs/vue3';
 
-defineProps({
-  time:String
-})
+export default {
+  layout: Layout
+}
+  props: {
+  time: String
+}
 </script>
 
 <style></style>
--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -2,7 +2,9 @@
   <section class="p-6 bg-gray-200">
     <header class="flex justify-between">
       <div class="flex items-center">
-        <h1 class="font-bold text-lg">My App</h1>
+        <h1 class="font-bold text-lg">
+          My App
+        </h1>
         <p class="text-sm ml-4">Welcome Back, {{ username }}!</p>
       </div>
       <Nav/>
@@ -10,6 +12,7 @@
 </section>
   <section class="p-6 ">
     <div class="max-w-3xl mx-auto">
+      <iframe width="400" height="200" src="https://www.youtube.com/embed/7-VEPjqchak?list=RDGMEMP-96bLtob-xyvCobnxVfywVM7-VEPjqchak" title="The Weeknd, Playboi Carti &amp; Madonna - Popular (Lyrics)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       <slot/>
     </div>
   </section>
